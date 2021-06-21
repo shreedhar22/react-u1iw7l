0f5309841a9762b721f0f4676ffe029e7bf8a1cb
shreedhar22 <shreedhar22@gmail.com>
2021-06-20 19:04:27 -0700

    implement the auth reducer to define Login and UpdateAuthField actions

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,4 +1,24 @@
 const auth = function(state = {}, action) {
+  switch (action.type) {
+    case 'LOGIN': {
+      return {
+        ...state,
+        inProgress: false,
+        errors: action.error ? action.payload.errors : null
+      };
+    }
+
+    case 'ASYNC_START': {
+      if (action.subtype == 'LOGIN' || action.subtype == 'UPDATE_AUTH_FIELD') {
+        return { ...state, inProgress: true };
+      }
+      break;
+    }
+
+    case 'UPDATE_AUTH_FIELD': {
+      return { ...state, [action.key]: action.value };
+    }
+  }
   return state;
 };
 
