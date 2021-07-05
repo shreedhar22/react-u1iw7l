5a377da58d01a6afe149ac183c8edb43548cf889
shreedhar22 <shreedhar22@gmail.com>
2021-07-04 18:02:00 -0700

    declare sessiosn storage actions in common reducer

--- src/reducers/common.js
+++ src/reducers/common.js
@@ -1,8 +1,20 @@
 const defaultState = {
-  appName: "Conduit"
+  appName: 'Conduit'
 };
 
-const common = function(state = defaultState, action) {
+const common = (state = defaultState, action) => {
+  // actions to store a session of the user logged in
+
+  switch (action.type) {
+    case 'APP_LOAD':
+      return {};
+
+    case 'REDIRECT':
+      return {};
+
+    case 'LOGIN':
+      return {};
+  }
   return state;
 };
 
