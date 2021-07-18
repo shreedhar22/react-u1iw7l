86196aa34e0c485037532259d1500cf8e2f2ef98
shreedhar22 <shreedhar22@gmail.com>
2021-07-18 12:49:02 -0700

    post call to create articles in agent; modify payload for same while dispatching

--- src/agent.js
+++ src/agent.js
@@ -16,7 +16,9 @@ const requests = {
 };
 
 const Articles = {
-  all: page => requests.get(`/articles?limit=10`)
+  all: page => requests.get(`/articles?limit=10`),
+  createArticle: (title, description) =>
+    requests.post(`/articles/create`, { article: { title, description } })
 };
 
 const Auth = {
