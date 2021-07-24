d053f1d997492b99407acc2b87c3071a6d4dbaa2
shreedhar22 <shreedhar22@gmail.com>
2021-07-23 21:29:30 -0700

    break after the sync start action

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,23 +1,20 @@
 const auth = function(state = {}, action) {
   switch (action.type) {
-    case 'LOGIN': {
+    case 'LOGIN':
       return {
         ...state,
         inProgress: false,
         errors: action.error ? action.payload.errors : null
       };
-    }
 
-    case 'ASYNC_START': {
+    case 'ASYNC_START':
       if (action.subtype == 'LOGIN' || action.subtype == 'UPDATE_AUTH_FIELD') {
         return { ...state, inProgress: true };
       }
       break;
-    }
 
-    case 'UPDATE_AUTH_FIELD': {
+    case 'UPDATE_AUTH_FIELD':
       return { ...state, [action.key]: action.value };
-    }
   }
   return state;
 };
