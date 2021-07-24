a49a50ee84e25fd2f5afb6f339493973d81f4d07
shreedhar22 <shreedhar22@gmail.com>
2021-07-24 00:47:29 -0700

    implement action types in Common reducer

--- src/reducers/common.js
+++ src/reducers/common.js
@@ -1,20 +1,31 @@
 const defaultState = {
-  appName: 'Conduit'
+  appName: 'Conduit',
+  token: 'null'
 };
 
 const common = (state = defaultState, action) => {
   // actions to store a session of the user logged in
 
-  // switch (action.type) {
-  //   case 'APP_LOAD':
-  //     return {};
+  switch (action.type) {
+    case 'APP_LOAD':
+      return {
+        ...state,
+        token: action.token || null,
+        appLoaded: true,
+        currentUser: action.payload ? actionpayload.user : null
+      };
 
-  //   case 'REDIRECT':
-  //     return {};
+    case 'REDIRECT':
+      return { ...state, redirectTo: null };
 
-  //   case 'LOGIN':
-  //     return {};
-  // }
+    case 'LOGIN':
+      return {
+        ...state,
+        redirectTo: action.error ? null : '/',
+        token: action.error ? null : action.payload.user.token,
+        currentUser: action.error ? null : action.payload.user
+      };
+  }
   return state;
 };
 
