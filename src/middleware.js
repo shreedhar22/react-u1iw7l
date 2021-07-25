cf05b0de84855826328f5493a98dc10e52f96098
shreedhar22 <shreedhar22@gmail.com>
2021-07-24 18:22:20 -0700

    undo the commit; as commit message isn't clear

--- src/middleware.js
+++ src/middleware.js
@@ -25,17 +25,4 @@ function isPromise(v) {
   return v && typeof v.then === 'function';
 }
 
-const localStorageMiddleware = store => next => action => {
-  if (action.type === 'LOGIN' || action.type === 'REGISTER') {
-    if (!action.error) {
-      window.localStorage.setItem('jwt', action.payload.user.token);
-      agent.setToken(action.payload.user.token);
-    } else if (action.type === 'LOGOUT') {
-      window.localStorage.setItem('jwt', '');
-      agent.setToken(null);
-    }
-  }
-  next(action);
-};
-
 export { promiseMiddleware, localStorageMiddleware };
