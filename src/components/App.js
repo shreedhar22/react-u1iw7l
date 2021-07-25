26bb0756d4c10630eb7e419f3ab8723a9814d0cf
shreedhar22 <shreedhar22@gmail.com>
2021-07-24 22:56:45 -0700

    comments to understand dispatching to props and mapping state to props better

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,25 @@ import '.././style.css';
 import { connect } from 'react-redux';
 import Header from './Header.js';
 
-// states(appName, currentUser and redirectTo(where)) are fetched from the // common reducer to attach to props
+// states(appName, currentUser and redirectTo(where)) are
+// fetched from the common reducer to attach to props
 const mapStateToProps = state => ({
   appName: state.common.appName,
   currentUser: state.common.currentUser,
   redirectTo: state.common.redirectTo
 });
 
-// actions(onLoad, onRedirect) are dispatched to props to store them in common reducer
+// actions(onLoad, onRedirect) are dispatched to
+// props to store them in common reducer(redux store)
 const mapDispatchToProps = dispatch => ({
   onLoad: (payload, token) => dispatch({ type: 'APP_LOAD', payload, token }),
   onRedirect: () => dispatch({ type: 'REDIRECT' })
 });
 
 class App extends React.Component {
+  // in order to dispatch the current user and token to
+  // props they are first fetched from the agent.
+  // onLoad is then dispatched within mapDispatchToProps
   componentWillMount() {
     const token = window.localStorage.getItem('jwt');
     if (token) {
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,8 @@
-import Banner from "./Banner";
-import MainView from "./MainView";
-import React from "react";
-import agent from "../../agent";
-import { connect } from "react-redux";
+import Banner from './Banner';
+import MainView from './MainView';
+import React from 'react';
+import agent from '../../agent';
+import { connect } from 'react-redux';
 
 const Promise = global.Promise;
 
@@ -10,11 +10,14 @@ const mapStateToProps = state => ({
   appName: state.common.appName
 });
 
+// onLoad action along with the payload is dispatched to props and stored in the redux store
 const mapDispatchToProps = dispatch => ({
-  onLoad: payload => dispatch({ type: "HOME_PAGE_LOADED", payload })
+  onLoad: payload => dispatch({ type: 'HOME_PAGE_LOADED', payload })
 });
 
 class Home extends React.Component {
+  // in order to dispatch articles to props they are fetched first from agent
+  // onLoad is then dispatched within mapDispatchToProps
   componentWillMount() {
     this.props.onLoad(agent.Articles.all());
   }
