a0222412824ae1ffbe224eecef0ea13cd67761fe
shreedhar22 <shreedhar22@gmail.com>
2021-08-08 14:20:47 -0700

    closing div

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,42 +1,32 @@
-import React from "react"
+import React from 'react';
 
 class Register extends React.Component {
-
-  render () {
-
+  render() {
     return (
       <div>
         <div className="auth-page">
-        <div className="container page">
-          <div className="row">
-            <div className="col-md-6 offset-md-3 col-xs-12">
-              <p className="text-xs-center">
-                <a>Need an account?</a>
-              </p>
-              <h1 className="text-xs-center">Sign UP</h1>
-            
-              
-              <form>
+          <div className="container page">
+            <div className="row">
+              <div className="col-md-6 offset-md-3 col-xs-12">
+                <p className="text-xs-center">
+                  <a>Need an account?</a>
+                </p>
+                <h1 className="text-xs-center">Sign UP</h1>
 
-                <fieldset>
-
+                <form>
                   <fieldset>
+                    <fieldset />
 
-                  </fieldset>
+                    <fieldset />
 
-                  <fieldset>
+                    <fieldset />
                   </fieldset>
-
-                  <fieldset>
-                    
-                  </fieldset>
-                </fieldset>
-              </form> 
+                </form>
               </div>
+            </div>
           </div>
-        </div> 
-
+        </div>
       </div>
-    )
+    );
   }
 }
