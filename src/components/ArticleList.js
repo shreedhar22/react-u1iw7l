e558df4382ea10e65db4239dcd2c1e923e49f345
shrpawar <shrpawar@cisco.com>
2021-10-13 09:50:59 -0700

    converting all function components into classes

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,22 +3,31 @@
 import ArticlePreview from "./ArticlePreview";
 import React from "react";
 
-const ArticleList = props => {
-  if (!props.articles) {
-    return <div className="article-preview">Loading...</div>;
+class ArticleList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.articles = props.articles;
   }
 
-  if (props.articles.length === 0) {
-    return <div className="article-preview">No articles are here... yet.</div>;
-  }
+  render() {
+    if (!this.articles) {
+      return <div className="article-preview">Loading...</div>;
+    }
 
-  return (
-    <div>
-      {props.articles.map(article => {
-        return <ArticlePreview article={article} key={article.slug} />;
-      })}
-    </div>
-  );
-};
+    if (this.articles.length === 0) {
+      return (
+        <div className="article-preview">No articles are here... yet.</div>
+      );
+    }
+
+    return (
+      <div>
+        {this.articles.map(article => {
+          return <ArticlePreview article={article} key={article.slug} />;
+        })}
+      </div>
+    );
+  }
+}
 
 export default ArticleList;
